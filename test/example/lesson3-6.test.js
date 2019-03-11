0e27ee46f38fed8359d955020578d5cbfe01905b
jeff <yinjiefeng@91ruler.com>
2019-03-11 10:07:55 +0800

    种花问题

--- test/example/lesson3-6.test.js
+++ test/example/lesson3-6.test.js
@@ -1,5 +1,5 @@
 import buildFlower from "../../code/lesson3/lesson3-6"
 
 test("build flower", () => {
-   expect(buildFlower([1,0,0,0,1,0,0,0,0,1], 2)).toBe(true);
+   expect(buildFlower([1,0,0,0,1,0,0,0,0,1], 2)).toBe(false);
 });
